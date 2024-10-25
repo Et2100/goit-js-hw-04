9dc42f85a88213a996666af7844955b40a5c323d
[Tommy Lew] <ttlewcz@gmail.com>
2024-10-25 13:25:08 +0100

    homework done

--- task2.js
+++ task2.js
@@ -1,35 +1,43 @@
 // Zadanie 2. Obliczanie kalorii
 
-// Napisz funkcję calcAverageCalories(days), która zwraca średnią dzienną wartość liczby kalorii, które sportowiec spożywał w ciągu tygodnia. Funkcja oczekuje jednego parametru: days — tablicy obiektów. Każdy obiekt opisuje dzień tygodnia oraz liczbę kalorii calories, spożytych przez sportowca tego dnia. Weź poniższy kod i wstaw go po deklaracji swojej funkcji, aby sprawdzić poprawność jej działania. W konsoli zostaną wyświetlone wyniki jej wywołań.
+// Napisz funkcję calcAverageCalories(days), która zwraca średnią dzienną wartość liczby kalorii, które sportowiec spożywał w ciągu tygodnia. Funkcja oczekuje jednego parametru: days — tablicy obiektów. Każdy obiekt opisuje dzień tygodnia oraz liczbę kalorii calories, spożytych przez sportowca tego dnia. 
 
 const calcAverageCalories = (days) => {
-    
-}
+    let total = 0;
+    let count = 0;
+    for (const day of days) {
+        total += day.calories;
+        count++;
+    };
+    return count > 0 ? total / count : 0;
+};
 
-// console.log(
-//   calcAverageCalories([
-//     { day: "monday", calories: 3010 },
-//     { day: "tuesday", calories: 3200 },
-//     { day: "wednesday", calories: 3120 },
-//     { day: "thursday", calories: 2900 },
-//     { day: "friday", calories: 3450 },
-//     { day: "saturday", calories: 3280 },
-//     { day: "sunday", calories: 3300 }
-//   ])
-// ); // 3180
+// Weź poniższy kod i wstaw go po deklaracji swojej funkcji, aby sprawdzić poprawność jej działania. W konsoli zostaną wyświetlone wyniki jej wywołań.
 
-// console.log(
-//   calcAverageCalories([
-//     { day: "monday", calories: 2040 },
-//     { day: "tuesday", calories: 2270 },
-//     { day: "wednesday", calories: 2420 },
-//     { day: "thursday", calories: 1900 },
-//     { day: "friday", calories: 2370 },
-//     { day: "saturday", calories: 2280 },
-//     { day: "sunday", calories: 2610 }
-//   ])
-// ); // 2270
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
 
-// console.log(
-//   calcAverageCalories([])
-// ); // 0
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
--- task3.js
+++ task3.js
@@ -7,9 +7,17 @@
 const profile = {
 	username: "Jacob",
   playTime: 300,
+
+changeUsername(newName){
+        this.username = newName
+    },
+updatePlayTime(hours){
+    this.playtime += hours
+},
+getInfo(){
+    return `${this.username} ma ${this.playTime} aktywnych godzin!`
+}
 };
-
-
 
 // Uzupełnij obiekt profile metodami do pracy z jego właściwościami.
 
@@ -22,10 +30,10 @@ const profile = {
 
 
 
-// console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-// profile.changeUsername("Marco");
-// console.log(profile.getInfo()); // "Marco has 300 active hours!"
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
-// profile.updatePlayTime(20);
-// console.log(profile.getInfo()); // "Marco has 320 active hours!"
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
